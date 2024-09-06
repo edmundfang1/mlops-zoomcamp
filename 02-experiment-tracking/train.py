2e17b30ca162e92f47fb7fd9dc3ca0fd39069a5f
edmundfang1 <edmundfang811@gmail.com>
2024-09-06 22:31:16 +0000

    added autologging

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -5,6 +5,8 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,15 +21,32 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        # set parameters & log them
+        max_depth=10
+        random_state=0
+
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("random_state", random_state)
+
+        rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        # calc & log rsme
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
+    
+    # setup mlflow
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("experiment-random-forest-regressor")
+
     run_train()
