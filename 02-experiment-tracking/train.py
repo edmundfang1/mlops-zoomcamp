cd1d5e0485b7fcdbb23b4068875b084346f73941
edmundfang1 <edmundfang811@gmail.com>
2024-09-14 22:19:54 +0000

    autolog train.py

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -21,26 +21,28 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    mlflow.autolog()
+
     with mlflow.start_run():
 
         X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
         X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
         # set parameters & log them
-        max_depth=10
-        random_state=0
+        # max_depth=10
+        # random_state=0
 
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_param("random_state", random_state)
+        # mlflow.log_param("max_depth", max_depth)
+        # mlflow.log_param("random_state", random_state)
 
-        rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
         # calc & log rsme
         rmse = mean_squared_error(y_val, y_pred, squared=False)
 
-        mlflow.log_metric("rmse", rmse)
+        # mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
